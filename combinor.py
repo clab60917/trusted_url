fc90d26bf698bb22035ca58d64cc079e843ef532
Clément <clab60917@eleve.isep.fr>
2024-05-15 10:42:01 +0200

    V4.1

--- combinor.py
+++ combinor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Définir les chemins des fichiers
-original_file = 'path_to_original_file.xlsx'  # fichier Excel original
+original_file = 'path_to_original_file.xlsx'  # Remplacez par le chemin de votre fichier Excel original
 output_file = 'output_domains_categories.xlsx'  # Le fichier généré par le deuxième script
 final_file = 'final_updated_file.xlsx'  # Le fichier Excel final avec les mises à jour
 
@@ -14,13 +14,13 @@ df_output = pd.read_excel(output_file)
 # Créer un dictionnaire pour une recherche rapide des catégories par domaine
 domain_to_category = dict(zip(df_output['Domain'], df_output['Category']))
 
-# Mettre à jour la colonne "categorie" dans le fichier original
+# Mettre à jour la colonne "Categorie" dans le fichier original
 def update_category(row):
     domain = row['domain']
-    return domain_to_category.get(domain, row['categorie'])  # Conserver la valeur existante si non trouvée
+    return domain_to_category.get(domain, row['Categorie'])  # Conserver la valeur existante si non trouvée
 
-# Ajouter une nouvelle colonne 'categorie' mise à jour
-df_original['categorie'] = df_original.apply(update_category, axis=1)
+# Ajouter une nouvelle colonne 'Categorie' mise à jour
+df_original['Categorie'] = df_original.apply(update_category, axis=1)
 
 # Enregistrer le fichier mis à jour dans un nouveau fichier Excel
 df_original.to_excel(final_file, index=False)
